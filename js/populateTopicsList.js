b1d1fea7a2c0ea43ae4d3baffe5226f89ed011d0
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-12-26 22:57:48 +0100

    Corrected LI creation with refactor

--- js/populateTopicsList.js
+++ js/populateTopicsList.js
@@ -4,14 +4,10 @@ const topicsListElem = document.querySelector('[data-topics]');
 
 export default function populateTopicsList() {
     sortedTopics.forEach(topic => {
-        const li = document.createElement('li');
-    
-        li.innerHTML = `
+        topicsListElem.innerHTML += `
             <li data-topic='${topic}'>
                 ${topic}
             </li>
         `;
-    
-        topicsListElem.append(li);
     });
 }
