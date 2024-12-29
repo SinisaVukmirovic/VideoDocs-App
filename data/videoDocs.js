a432cd2f72ca1067525af8f0cd303f6b79cdabcc
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-12-29 16:19:00 +0100

    Cganged Data to suit new card design

--- data/videoDocs.js
+++ data/videoDocs.js
@@ -3,51 +3,79 @@ export { videoDocsData };
 const videoDocsData = [
     {
         videoUrl: 'https://www.youtube.com/watch?v=6aHKdahOfCc',
-        thumbnail: 'https://i.ytimg.com/an_webp/6aHKdahOfCc/mqdefault_6s.webp?du=3000&sqp=CMXqqrsG&rs=AOn4CLCJ3_K6in2M7st4-97kTQgDPrlLKA',
         topic: 'height',
         title: 'One of the most common CSS issues people run into',
-        author: 'Kevin Powell'
+        author: 'Kevin Powell',
+        duration: '6m'
     },
     {
         videoUrl: 'https://www.youtube.com/watch?v=k7-N8R0-KY4',
-        thumbnail: 'https://i.ytimg.com/an_webp/k7-N8R0-KY4/mqdefault_6s.webp?du=3000&sqp=COKbq7sG&rs=AOn4CLD5v_oPiRDAmjTDd-vhL9ySmXZc3g',
         topic: 'Recursion',
         title: 'Recursion - Part 7 of Functional Programming in JavaScript',
-        author: 'Fun Fun Function'
+        author: 'Fun Fun Function',
+        duration: '16m'
     },
     {
         videoUrl: 'https://www.youtube.com/watch?v=sT8UlV_Ku0s',
-        thumbnail: 'https://i.ytimg.com/an_webp/sT8UlV_Ku0s/mqdefault_6s.webp?du=3000&sqp=CJmSq7sG&rs=AOn4CLAa_A9mLv3ICX4UgUgFENUkCJb_vw',
         topic: 'subgrid',
         title: 'CSS Subgrid Layouts Are Here!',
-        author: 'Dmitry Mayorov'
+        author: 'Dmitry Mayorov',
+        duration: '9m'
     },
     {
         videoUrl: 'https://www.youtube.com/watch?v=wgYx_8SoiUY',
-        thumbnail: 'https://i.ytimg.com/vi/wgYx_8SoiUY/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDJB0m8iI3QN1ORsuNO2P-C6m4AeQ',
         topic: 'Clean code',
         title: 'How to write clean code',
-        author: 'Scrimba'
+        author: 'Dylan Israel',
+        duration: '1h 4m'
     },
     {
         videoUrl: 'https://www.youtube.com/watch?v=zNXQS2DfckU',
-        thumbnail: 'https://i.ytimg.com/an_webp/zNXQS2DfckU/mqdefault_6s.webp?du=3000&sqp=CMz2trsG&rs=AOn4CLBuWEpsIoxtb-E03mr1GkzlJgcjEg',
         topic: 'Three.js',
         title: 'Amazing 3D Animation Website with Three.Js So Eazy',
-        author: 'Lun Dev Code'
+        author: 'Lun Dev Code',
+        duration: '15m'
     },
     {
         videoUrl: 'https://www.youtube.com/watch?v=zNXQS2DfckU',
-        thumbnail: 'https://i.ytimg.com/an_webp/JN-nme9oF10/mqdefault_6s.webp?du=3000&sqp=CM_WvLsG&rs=AOn4CLCb13cL0ao3MPwpcncH6PAfqOw5VA',
         topic: 'GSAP',
         title: 'Amazing 3D Animation Website with Three.Js So Eazy',
-        author: 'Lun Dev Code'
+        author: 'Lun Dev Code',
+        duration: '15m'
     },
     {
         videoUrl: 'https://www.youtube.com/watch?v=fF_NVrd1s14',
-        thumbnail: 'https://i.ytimg.com/an_webp/fF_NVrd1s14/mqdefault_6s.webp?du=3000&sqp=CJiwvLsG&rs=AOn4CLBqBkdE9Wx5FP188f18u2mTNeel5g',
         topic: 'position',
         title: 'CSS position deep dive',
-        author: 'Kevin Powell'
+        author: 'Kevin Powell',
+        duration: '28m'
+    },
+    {
+        videoUrl: 'https://www.youtube.com/watch?v=fF_NVrd1s14',
+        topic: 'Media query',
+        author: 'Kevin Powell',
+        title: 'Learn how to use Media queries & Container queries',
+        duration: '35m'
+    },
+    {
+        videoUrl: 'https://www.youtube.com/watch?v=fF_NVrd1s14',
+        topic: 'Container query',
+        author: 'Kevin Powell',
+        title: 'Learn how to use Media queries & Container queries',
+        duration: '35m'
+    },
+    {
+        videoUrl: 'https://www.youtube.com/watch?v=SpfTjCy_PKM',
+        topic: 'Media query',
+        author: 'IFace',
+        title: 'Each Media Query in CSS You MUST Know',
+        duration: '5m'
+    },
+    {
+        videoUrl: 'https://www.youtube.com/watch?v=JY0FN71vCbw',
+        topic: 'display',
+        author: 'Web Dev Simplified',
+        title: 'The CSS Display Property is Changing Forever',
+        duration: '15m'
     },
 ];
