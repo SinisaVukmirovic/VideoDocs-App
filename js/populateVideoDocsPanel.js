3541c5a4d06a33b90b9a28046304caef730689b7
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-12-30 14:25:14 +0100

    tryed new colors

--- js/populateVideoDocsPanel.js
+++ js/populateVideoDocsPanel.js
@@ -8,7 +8,9 @@ export default function populateVideoDocsPanel() {
                 <h3 class="topic">${data.topic}</h3>
                 <h4 class="author">by ${data.author}</h4>
                 <h3 class="title">${data.title}</h3>
-                <h5 class="duration">duration: ${data.duration}</h5>
+                <h5 class="duration">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="var(--secondary-text)"><path d="M12.5 7.25a.75.75 0 00-1.5 0v5.5c0 .27.144.518.378.651l3.5 2a.75.75 0 00.744-1.302L12.5 12.315V7.25z"></path><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"></path></svg> ${data.duration}
+                </h5>
             </a>
         `;
     });
